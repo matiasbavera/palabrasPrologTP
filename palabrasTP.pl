cf2155bc371e4494b63ba3020b64e2f097d10915
matias bavera <matiasbavera@gmail.com>
2016-05-12 19:46:46 -0400

    final sin setof
    para correr fin(R).

--- palabrasTP.pl
+++ palabrasTP.pl
@@ -30,12 +30,13 @@ pista(5, ['b', 'a', 's', 't', 'a'], 2).
 pista(5, ['c', 'o', 'r', 't', 'e'], 2).
 pista(5, ['c', 'o', 'p', 'l', 'a'], 3).
 
-%sexta matriz con las palabras encontradas. Esta aca con el fin de probar encontrar su palabra oculta.(no sale ): )
-pista(6, ['c', 'a', 'l', 'd', 'o'], 1).
-pista(6, ['c', 'e', 'r', 'd', 'a'], 2).
-pista(6, ['b', 'a', 'l', 'd', 'e'], 2).
-pista(6, ['s', 'e', 'l', 'l', 'o'], 3).
-pista(6, ['b', 'o', 'r', 'l', 'a'], 3).
+pista(6, [], 1).
+pista(6, [], 2).
+pista(6, [], 2).
+pista(6, [], 3).
+pista(6, [], 3).
+
+
 
 % Cargar pistas
 cantidad_pistas_palabra(Bloque, R):-bagof(R, B^pista(Bloque, B, R), R).
@@ -59,11 +60,10 @@ comparar([X|L],[M|N], R):-
 	comparar(L,N,R),!.
 
 
-resolver(X,M):- %X el numero de la matriz, M palabra encontrada
+resolver(X,M):-
 	lista_pistas_palabra(X, P),
 	cantidad_pistas_palabra(X, K),
 	clpfd:transpose(P, T),
-
 	% Pistas
 	P = [F1, F2, F3, F4, F5],
 	% Pistas transpuestas
@@ -85,3 +85,42 @@ resolver(X,M):- %X el numero de la matriz, M palabra encontrada
 	comparar(M, F3, K3),
 	comparar(M, F4, K4),
 	comparar(M, F5, K5).
+
+
+resolver2(X,Z,M):-
+	%lista_pistas_palabra(X, P),
+	%write(Z),
+	cantidad_pistas_palabra(X, K),
+	clpfd:transpose(Z, T),
+	% Pistas
+	Z = [F1, F2, F3, F4, F5],
+	% Pistas transpuestas
+	T = [T1, T2, T3, T4, T5],
+	% Cantidad por fila
+	K = [K1, K2, K3, K4, K5],
+
+	% Iterar sobre las letras correspondientes
+	asignar_letra(T1, A),
+	asignar_letra(T2, B),
+	asignar_letra(T3, C),
+	asignar_letra(T4, D),
+	asignar_letra(T5, E),
+	M=[A, B, C, D, E],
+	%write(M),write("\n"),
+	% Validar que las letras son correctas
+	comparar(M, F1, K1),
+	comparar(M, F2, K2),
+	comparar(M, F3, K3),
+	comparar(M, F4, K4),
+	comparar(M, F5, K5).
+
+
+fin(R):-
+	resolver(1,N1),
+	resolver(2,N2),
+	resolver(3,N3),
+	resolver(4,N4),
+	resolver(5,N5),
+	N = [N1,N2,N3,N4,N5],
+	write(N),
+	resolver2(6,N,R).
